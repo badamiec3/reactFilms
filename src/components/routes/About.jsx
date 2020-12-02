f4bafcff6734f9f917a847c04c1472989770b20b
badamiec3 <71398646+badamiec3@users.noreply.github.com>
2020-12-02 12:59:44 +0000

    Create About.jsx

---
+++ src/components/routes/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return(
+      <>
+          <h1> About Films </h1>
+          <p>Films is an extensive database for information about your favourite films.</p>
+          <p>Simply enter the title of the film you would like to look up and click 'Search'.</p>
+          <p>Feel free to use your own API key. One is already provided for you.</p>
+      </>
+  );
+}
+export default About;
